ddb49247f24c25f78f9e3986ba205df55713685b
Anurag Sharan <sharananurag998@gmail.com>
2025-11-12 03:03:49 +0530

    Remove test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { TestEditableText } from './TestEditableText'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <TestEditableText />
       {/* Original placeholder - you can also double-click to edit this! */}
       <div className="p-8 text-center">
         <p className="text-gray-600">Start prompting (or editing) to see magic happen :)</p>
